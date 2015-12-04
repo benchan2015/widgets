662b1e2ecd07332e2a1963dca9b592c29fc680e8
benchan2015 <421416050@qq.com>
2015-12-04 14:02:11 +0800

    init version

---
+++ gulp/tasks/build.js
@@ -0,0 +1,21 @@
+var gulp=require('gulp');
+var sass = require('gulp-sass');
+
+var runSequence = require('run-sequence');
+
+//处理sass
+gulp.task('sass', function() {
+    return gulp.src(['./src/sass/**/*.scss'])
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./build/css'));
+});
+
+//处理sass
+gulp.task('build', function() {
+     gulp.src(['./src/demo/**'])
+        .pipe(gulp.dest('./build/demo'));
+    gulp.src(['./src/lib/**'])
+        .pipe(gulp.dest('./build/javascripts'));
+    runSequence(['sass']);
+
+});
